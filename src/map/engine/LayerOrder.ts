d68025e2863af2dd0a83c76c7dcd56b89da235aa
Sergek888 <72980648+Sergek888@users.noreply.github.com>
2026-06-25 15:41:59 +0400

    Remove unused map engine draft files

--- src/map/engine/LayerOrder.ts
+++
@@ -1,9 +0,0 @@
-import type { LayerSpecification } from 'maplibre-gl'
-
-export const MAP_LAYER_ANCHORS = {
-  baseEnd: 'anchor-base-end', reliefEnd: 'anchor-relief-end', overlayEnd: 'anchor-overlay-end', routeOverlayEnd: 'anchor-route-overlay-end', trackEnd: 'anchor-track-end', markerEnd: 'anchor-marker-end', interactionEnd: 'anchor-interaction-end', tooltipEnd: 'anchor-tooltip-end',
-} as const
-
-export function createAnchorLayers(): LayerSpecification[] {
-  return Object.values(MAP_LAYER_ANCHORS).map((id) => ({ id, type: 'background', paint: { 'background-opacity': 0 } }))
-}
