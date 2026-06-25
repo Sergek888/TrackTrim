969f7326a54724bbc8c295e9f31cb43c4e64797a
Sergek888 <72980648+Sergek888@users.noreply.github.com>
2026-06-25 14:26:24 +0400

    Adjust old layer availability shim

--- src/map/model/MapLayerAvailability.ts
+++ src/map/model/MapLayerAvailability.ts
@@ -2,4 +2,8 @@ export interface MapLayerAvailabilitySettings {
   availableLayerIds: string[]
   showExperimentalLayers: boolean
   showFragileLayers: boolean
+  enabledLayerIds?: string[]
+  hiddenLayerIds?: string[]
+  enabledGroupIds?: string[]
 }
+
