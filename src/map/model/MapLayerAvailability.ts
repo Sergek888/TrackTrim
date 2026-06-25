f3448715e753bfb509a2da4c63f4485ea39fb098
Sergek888 <72980648+Sergek888@users.noreply.github.com>
2026-06-25 14:26:39 +0400

    Keep old availability fields required

--- src/map/model/MapLayerAvailability.ts
+++ src/map/model/MapLayerAvailability.ts
@@ -1,9 +1,8 @@
 export interface MapLayerAvailabilitySettings {
   availableLayerIds: string[]
+  enabledLayerIds: string[]
+  hiddenLayerIds: string[]
+  enabledGroupIds: string[]
   showExperimentalLayers: boolean
   showFragileLayers: boolean
-  enabledLayerIds?: string[]
-  hiddenLayerIds?: string[]
-  enabledGroupIds?: string[]
 }
-
